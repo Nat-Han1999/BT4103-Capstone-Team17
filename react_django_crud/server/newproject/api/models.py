2bc14af773dfbf33e93cf42b5d43dbb17c14e22b
shanicesng <shanicesng@u.nus.edu>
2024-09-08 13:12:33 +0800

    removed irrelavent folders from avatar code

--- react_django_crud/server/newproject/api/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-class Book(models.Model): #Input accepts a model
-    title = models.CharField(max_length=50) # Defining a string in Django
-    release_year = models.IntegerField() # Can be rep as an integer
-
-    # Define how the class will be represented
-    def __str__(self):
-        return self.title # Output when you print a Book
