b6545d62b8d35e3c5ae67b8ff6ecb0a154452156
shanicesng <shanicesng@u.nus.edu>
2024-09-22 23:52:59 +0800

    mui dropdown bar for avatar and bg change

--- basic_avatar/src/App.jsx
+++ basic_avatar/src/App.jsx
@@ -1,6 +1,14 @@
 import { Canvas } from "@react-three/fiber";
 import { Experience } from "./components/Experience";
 import { BackgroundDropdown } from "./components/BackgroundDropdown";
+import {
+  Box,
+  Stack,
+  InputLabel,
+  FormControl,
+  NativeSelect,
+  formHelperTextClasses,
+} from "@mui/material";
 import { Html } from "@react-three/drei";
 import { useState, useEffect } from "react";
 
@@ -33,34 +41,56 @@ function App() {
           pointerEvents: "auto", // Allow pointer events
         }}
       >
-        <select
-          value={avatarBG}
-          onChange={(e) => {
-            setAvatarBG(e.target.value);
+        <Box
+          p={3}
+          sx={{
+            width: 150,
+            height: 150,
+            borderRadius: 2,
+            display: "flex",
+            bgcolor: "white",
+            justifyContent: "center",
+            alignItems: "center",
+            color: "white",
           }}
         >
-          <option value="avatar_bg">Default</option>
-          <option value="avatar_bg2">Seaside</option>
-          <option value="avatar_bg3">Desert</option>
-          <option value="avatar_bg4">Space</option>
-        </select>
+          <Stack spacing={3}>
+            <FormControl fullWidth>
+              <InputLabel variant="standard">Background Image</InputLabel>
+              <NativeSelect
+                defaultValue={"avatar_bg"}
+                onChange={(e) => {
+                  setAvatarBG(e.target.value);
+                }}
+              >
+                <option value={"avatar_bg"}>Default</option>
+                <option value={"avatar_bg2"}>Seaside</option>
+                <option value={"avatar_bg3"}>Desert</option>
+                <option value={"avatar_bg4"}>Space</option>
+              </NativeSelect>
+            </FormControl>
 
-        <select
-          value={avatarLook}
-          onChange={(e) => {
-            setAvatarLook(e.target.value);
-            if (e.target.value == "avatar_chinese_lady") {
-              setAvatarScript("hello");
-            } else if (e.target.value == "avatar_indian_man") {
-              setAvatarScript("niraj");
-            } else {
-              setAvatarScript("hello");
-            }
-          }}
-        >
-          <option value="avatar_chinese_lady">Chinese Lady</option>
-          <option value="avatar_indian_man">Indian Man</option>
-        </select>
+            <FormControl fullWidth>
+              <InputLabel variant="standard">Avatar</InputLabel>
+              <NativeSelect
+                defaultValue={"avatar_chinese_lady"}
+                onChange={(e) => {
+                  setAvatarLook(e.target.value);
+                  if (e.target.value == "avatar_chinese_lady") {
+                    setAvatarScript("hello");
+                  } else if (e.target.value == "avatar_indian_man") {
+                    setAvatarScript("niraj");
+                  } else {
+                    setAvatarScript("hello");
+                  }
+                }}
+              >
+                <option value={"avatar_chinese_lady"}>Chinese Lady</option>
+                <option value={"avatar_indian_man"}>Indian Man</option>
+              </NativeSelect>
+            </FormControl>
+          </Stack>
+        </Box>
       </Html>
       <color attach="background" args={["#ececec"]} />
       <Experience
