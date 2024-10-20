46513e29afb9887057ac61f6e5fd59691608f25d
Max <maxskyong@gmail.com>
2024-10-20 23:00:21 +0800

    Testing Pipeline + Readability Scores
    Implemented automated execution of test cases upon local server deployment. Comment out "request_finished.connect(runserver_test_trigger)" in views.py if you want fast local server deployment without test cases. Update test_cases.py to add new cases if needed. Added calculation and recording of Flesch Kincaid readability scores (calculated per conversation - one replicate). Results recorded in test_case_x.json files.

---
+++ llm (django)/chatbotapp/test_cases.py
@@ -0,0 +1,25 @@
+tests = [
+    [
+        "I'm a 30 year old male that has recently been fired, I believe I've been wrongly dismissed and will need some help answering some questions regarding what the Shrama Vasana fund can do for me, can you help me if I provide some questions I have?",
+        "What documents will I need to submit?",
+        "How can I start the application process?",
+        "Just to confirm that you're understanding, can you repeat back to me what my demographic is?",
+        "If my salary was RS 40000, then I'm not eligible for legal aid?"
+    ],
+    [
+        "What services can I get from Shrama Vasana fund?",
+        "What kind of worker welfare?"
+    ],
+    [
+        "My work place closed down without prior notice.",
+        "How do I sign up for these courses?",
+        "Where do I get financial aid?"
+    ],
+    [
+        "What services are supported by donations to the Shrama Vasana Fund?",
+        "What kind of scholarships does the Shrama Vasana Fund provide?",
+        "How does the Shrama Vasana Fund help workers who lose their jobs without notice?",
+        "What legal aid does the Shrama Vasana Fund provide to terminated employees?",
+        "How can I contribute to the Shrama Vasana Fund?",
+    ],
+]
