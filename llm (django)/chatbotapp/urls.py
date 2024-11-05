76a53a0c04885098ae6017ee7b7296bb19f05a19
Nat-Han1999 <88693280+Nat-Han1999@users.noreply.github.com>
2024-11-05 17:08:04 +0800

    Delete llm (django) directory

--- llm (django)/chatbotapp/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import send_message, list_messages, clear_messages
-
-urlpatterns = [
-    path('send', send_message, name='send_message'),
-    path('clear_messages', clear_messages, name='clear_messages'),
-    path('', list_messages, name='list_messages'),
-]
